588a770a2bee4ff4993c8765c7c68ea381a06ca6
mjjh <1692762315@qq.com>
2023-04-10 22:15:12 +0800

    wip: 修改 vercel 配置文件

--- api/proxy.js
+++ api/proxy.js
@@ -1,13 +1,28 @@
-import { createProxyMiddleware } from 'http-proxy-middleware'
+/*
+ * @Author: mjjh
+ * @LastEditTime: 2023-04-10 22:13:36
+ * @FilePath: \chagpt-shuowen\api\proxy.js
+ * @Description: vercel 重写函数配置
+ */
+import http from 'http'
 
 export default (req, res) => {
-  let target = ''
-  if (req.url.startsWith('/api'))
-    target = process.env.VITE_APP_API_BASE_URL
+  if (req.url.startsWith('/api')) {
+    const target = process.env.VITE_APP_API_BASE_URL
+    const options = {
+      target,
+      changeOrigin: true,
+      pathRewrite: path => path.replace('/api', ''),
+    }
 
-  createProxyMiddleware({
-    target,
-    changeOrigin: true,
-    pathRewrite: path => path.replace('/api', '/'),
-  })(req, res)
+    const proxyReq = http.request(options, (proxyRes) => {
+      res.writeHead(proxyRes.statusCode, proxyRes.headers)
+      proxyRes.pipe(res, { end: true })
+    })
+
+    req.pipe(proxyReq, { end: true })
+  }
+  else {
+    res.status(404).send('Not Found')
+  }
 }
