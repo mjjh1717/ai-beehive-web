617d20d4c346953c3ac09aa7c72a24abcdc54bad
mjjh <1692762315@qq.com>
2023-04-10 22:56:36 +0800

    wip: 修改 vercel 流式输出

--- api/proxy.js
+++ api/proxy.js
@@ -1,41 +1,40 @@
-const { createProxyMiddleware } = require('http-proxy-middleware')
-module.exports = (req, res) => {
-  let apiTarget = ''
-  if (req.url.startsWith('/api'))
-    apiTarget = process.env.VITE_APP_API_BASE_URL
+import { createProxyMiddleware } from 'http-proxy-middleware'
 
-  const myProxy = createProxyMiddleware({
-    target: apiTarget,
+export default (req, res) => {
+  let target = ''
+  if (req.url.startsWith('/api'))
+    target = process.env.VITE_APP_API_BASE_URL
+
+  const proxy = createProxyMiddleware({
+    target,
     changeOrigin: true,
-    selfHandleResponse: true,
     pathRewrite: path => path.replace('/api', '/'),
-    onProxyRes(proxyRes, req, res) {
-      proxyRes.pipe(res)
+    // 添加 proxy_buffering off 参数
+    onProxyReq: (proxyReq) => {
+      proxyReq.setHeader('proxy_buffering', 'off')
     },
   })
 
-  // 添加 Nginx 配置项
-  const nginxConfig = {
-    '/api': {
-      ...myProxy,
-      headers: {
-        proxy_buffering: 'off',
-      },
-    },
+  // 设置响应头，表明响应数据的类型是流式的
+  res.setHeader('Content-Type', 'application/octet-stream')
+
+  // 覆盖 res.write() 方法，逐步将响应数据写入到客户端
+  const originalWrite = res.write
+  res.write = function (chunk, encoding, callback) {
+    // 向客户端发送当前的数据块
+    res.flushHeaders()
+    originalWrite.call(res, chunk, encoding, callback)
   }
 
-  // 根据请求路径获取对应的代理函数和 Nginx 配置项
-  const { target, ...nginxOpts } = nginxConfig[req.url]
-
-  // 如果没有找到，则返回 404 错误
-  if (!target) {
-    res.status(404).send('Not Found')
-    return
-  }
-
-  // 设置透明代理模式
-  myProxy(req, res, () => {
-    res.writeHead(404)
+  // 调用代理中间件，并将其响应数据转换为流式输出
+  proxy(req, res, (err) => {
+    if (err) {
+      console.error(err)
+      // 发送错误信息到客户端
+      res.statusCode = 500
+      res.end('Internal Server Error')
+    }
+    // 标志着所有的响应数据已经全部发送完毕
     res.end()
   })
 }
