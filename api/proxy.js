610e7cca9c1c9f9e8b9b329c26e58300ee1e2292
mjjh <1692762315@qq.com>
2023-04-10 22:29:47 +0800

    wip: 添加vercel流式输出规则

--- api/proxy.js
+++ api/proxy.js
@@ -1,28 +1,17 @@
-/*
- * @Author: mjjh
- * @LastEditTime: 2023-04-10 22:13:36
- * @FilePath: \chagpt-shuowen\api\proxy.js
- * @Description: vercel 重写函数配置
- */
-import http from 'http'
+import { createProxyMiddleware } from 'http-proxy-middleware'
 
 export default (req, res) => {
-  if (req.url.startsWith('/api')) {
-    const target = process.env.VITE_APP_API_BASE_URL
-    const options = {
-      target,
-      changeOrigin: true,
-      pathRewrite: path => path.replace('/api', ''),
-    }
+  let target = ''
+  if (req.url.startsWith('/api'))
+    target = process.env.VITE_APP_API_BASE_URL
 
-    const proxyReq = http.request(options, (proxyRes) => {
-      res.writeHead(proxyRes.statusCode, proxyRes.headers)
-      proxyRes.pipe(res, { end: true })
-    })
-
-    req.pipe(proxyReq, { end: true })
-  }
-  else {
-    res.status(404).send('Not Found')
-  }
+  createProxyMiddleware({
+    target,
+    changeOrigin: true,
+    pathRewrite: path => path.replace('/api', '/'),
+    selfHandleResponse: true,
+    onProxyRes(proxyRes, req, res) {
+      proxyRes.pipe(res)
+    },
+  })(req, res)
 }
