d9a03c92c09841e911ba9da7abc0df9b56a7ad6d
mjjh <1692762315@qq.com>
2023-04-10 22:41:04 +0800

    wip: 修改 vercel 函数配置

--- api/proxy.js
+++ api/proxy.js
@@ -1,17 +1,40 @@
-import { createProxyMiddleware } from 'http-proxy-middleware'
-
-export default (req, res) => {
-  let target = ''
+const { createProxyMiddleware } = require('http-proxy-middleware')
+module.exports = (req, res) => {
+  let apiTarget = ''
   if (req.url.startsWith('/api'))
-    target = process.env.VITE_APP_API_BASE_URL
+    apiTarget = process.env.VITE_APP_API_BASE_URL
 
-  createProxyMiddleware({
-    target,
+  const myProxy = createProxyMiddleware({
+    target: apiTarget,
     changeOrigin: true,
-    pathRewrite: path => path.replace('/api', '/'),
     selfHandleResponse: true,
     onProxyRes(proxyRes, req, res) {
       proxyRes.pipe(res)
     },
-  })(req, res)
+  })
+
+  // 添加 Nginx 配置项
+  const nginxConfig = {
+    '/api': {
+      ...myProxy,
+      headers: {
+        proxy_buffering: 'off',
+      },
+    },
+  }
+
+  // 根据请求路径获取对应的代理函数和 Nginx 配置项
+  const { target, ...nginxOpts } = nginxConfig[req.url]
+
+  // 如果没有找到，则返回 404 错误
+  if (!target) {
+    res.status(404).send('Not Found')
+    return
+  }
+
+  // 设置透明代理模式
+  myProxy(req, res, () => {
+    res.writeHead(404)
+    res.end()
+  })
 }
